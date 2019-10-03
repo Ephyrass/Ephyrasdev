97b1d10bdc9cda50c1ac0e37ad9ac4c0a0313a83
Ephyrass <jb.houi@yahoo.fr>
2019-10-03 13:06:38 +0200

    deploy

--- server.js
+++ server.js
@@ -11,13 +11,12 @@ app.use(express.json());
 app.use("/", index);
 
 // Serve static asset if in prod
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
