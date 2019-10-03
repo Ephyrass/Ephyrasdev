5af042e96ee1054f40f9a29476b5e8236b5128fe
Ephyrass <jb.houi@yahoo.fr>
2019-10-03 11:38:54 +0200

    version 1

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("server is running");
 });
 
